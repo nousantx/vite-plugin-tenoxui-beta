b6363b873100d054f5c59cefa62022bc4d915784
nousantx <nousantx@gmail.com>
2025-05-25 10:44:53 +0700

    feat: initial commit...

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import x from './plugin.js'
+import i from 'vite-plugin-inspect'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), x(), i()],
+})
---
+++ x.js
@@ -0,0 +1,6 @@
+import { createHotContext as __vite__createHotContext } from "/@vite/client";import.meta.hot = __vite__createHotContext("/@id/virtual:tenoxui.css");import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from "/@vite/client"
+const __vite__id = "virtual:tenoxui.css"
+const __vite__css = ".size-50 {\n  width: 12.5rem;\n  height: 12.5rem;\n}\n.rounded-md {\n  border-radius: 0.375rem;\n}\n.bg-red {\n  background-color: red;\n}"
+__vite__updateStyle(__vite__id, __vite__css)
+import.meta.hot.accept()
+import.meta.hot.prune(() => __vite__removeStyle(__vite__id))
